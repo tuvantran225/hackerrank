3211c8e30741abc878d6aa40d5ed05af44690367
frank.tran <frank.tran@codews079.code88.asia>
2018-09-21 17:43:18 +0700

    Update

---
+++ src/hackerrank/JavaStaticInitializerBlock.java
@@ -0,0 +1,14 @@
+/*
+ * https://www.hackerrank.com/challenges/java-static-initializer-block/problem
+ */
+package hackerrank;
+
+/**
+ *
+ * @author frank.tran
+ */
+public class JavaStaticInitializerBlock {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ test/Test.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author frank.tran
+ */
+public class Test {
+    private static int a = 0;
+    public static void main(String[] args) {
+        int b = 1;
+    }
+}
